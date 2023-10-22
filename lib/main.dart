ebdbd36c3812d1b6f72ddfe43cb22d3d7d5820de
KKNMASTER <k.sakmongkoljit@gmail.com>
2023-10-22 21:17:45 +0700

    KKN Homework Flutter V2

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class _SumOfNumbersScreenState extends State<SumOfNumbersScreen> {
   TextEditingController num3Controller = TextEditingController();
   TextEditingController num4Controller = TextEditingController();
   String result = '';
+  String result2 = '';
 
   void calculateSum() {
     setState(() {
@@ -31,7 +32,26 @@ class _SumOfNumbersScreenState extends State<SumOfNumbersScreen> {
       int num4 = int.tryParse(num4Controller.text) ?? 0;
 
       result = 'Sum: ${num1 + num2 + num3 + num4}';
-    });
+     });
+  }
+  void calculateSumeven() {
+    setState(() {
+      int num1 = int.tryParse(num1Controller.text) ?? 0;
+      int num2 = int.tryParse(num2Controller.text) ?? 0;
+      int num3 = int.tryParse(num3Controller.text) ?? 0;
+      int num4 = int.tryParse(num4Controller.text) ?? 0;
+
+      List<int> evennum = [num1, num2, num3, num4];
+      List<int> evenNumbers = [];
+      int sumeven = 0;
+      for (int numbers in evennum) {
+      if (numbers %2 == 0) {
+      evenNumbers.add(numbers);}}
+      for (int numbers in evenNumbers) {
+      sumeven += numbers;
+      result2 = '$sumeven';
+      }
+      });
   }
 
   @override
@@ -42,37 +62,46 @@ class _SumOfNumbersScreenState extends State<SumOfNumbersScreen> {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: num1Controller,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Enter number 1'),
-            ),
-            TextField(
-              controller: num2Controller,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Enter number 2'),
-            ),
-            TextField(
-              controller: num3Controller,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Enter number 3'),
-            ),
-            TextField(
-              controller: num4Controller,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Enter number 4'),
-            ),
-            ElevatedButton(
-              onPressed: calculateSum,
-              child: Text('Calculate Sum'),
-            ),
-            Text(
-              result,
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: num1Controller,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Enter number 1'),
+              ),
+              TextField(
+                controller: num2Controller,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Enter number 2'),
+              ),
+              TextField(
+                controller: num3Controller,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Enter number 3'),
+              ),
+              TextField(
+                controller: num4Controller,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Enter number 4'),
+              ),
+              ElevatedButton(
+                onPressed: calculateSum,
+                child: Text('Calculate Sum'),
+              ),
+              Text(
+                result,
+                style: TextStyle(fontSize: 20),
+              ),ElevatedButton(
+                onPressed: calculateSumeven,
+                child: Text('Sum even'),
+              ),
+              Text(
+                result,
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
         ),
       ),
     );
